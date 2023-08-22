e6bf873629a346885562a78abc14a3028d9cd03b
SieuBuong <huycloud1999@gmail.com>
2023-08-22 23:10:41 +0700

    done

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,7 @@ import { authenticateToken } from "../middlewares/index.js";
 import { SignIn, getAllProducts, getProductDescriptions, getProductLow } from "../controller/index.js";
 const CombineRouter =Router();
 CombineRouter.get('/products',authenticateToken,getAllProducts)
-CombineRouter.get('/products/low-quantity',getProductLow)
-CombineRouter.get('/products/orders-with-products',getProductDescriptions)
+CombineRouter.get('/products/low-quantity',authenticateToken,getProductLow)
+CombineRouter.get('/products/orders-with-products',authenticateToken,getProductDescriptions)
 CombineRouter.post('/signin',SignIn)
 export default CombineRouter
